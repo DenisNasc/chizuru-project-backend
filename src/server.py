23823fb2815ea6e7c4d8898a07d440baa0f475b7
Denis <devdenisbr@gmail.com>
2020-12-25 21:58:47 -0300

    Added python-dotenv

--- src/server.py
+++ src/server.py
@@ -1,3 +1,5 @@
+from os import getenv
+from settings import BASE_DIR
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import BaseQuery, SQLAlchemy
@@ -6,9 +8,18 @@ from flask_sqlalchemy import BaseQuery, SQLAlchemy
 from routes.RouteUsers import create_route_users
 from routes.RouteProjects import create_route_projects
 
+IS_DEVELOPMENT = getenv('IS_DEVELOPMENT')
+
+if IS_DEVELOPMENT:
+    DEVELOPMENT_DATABASE_URI = getenv('DEVELOPMENT_DATABASE_URI')
+    DATABASE_URI = f'sqlite:///{BASE_DIR}/{DEVELOPMENT_DATABASE_URI}'
+
+else:
+    DATABASE_URI = BASE_DIR + getenv('PRODUCTION_DATABASE_URI')
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 
 api = Api(app)
 db = SQLAlchemy(app=app, query_class=BaseQuery)
@@ -17,6 +28,11 @@ api.add_resource(create_route_users(db), "/users/<string:user_id>")
 api.add_resource(create_route_projects(db),
                  "/users/<string:user_id>/projects/<string:project_id>")
 
-if __name__ == "__main__":
+
+def main():
     db.create_all()  # VERIFICAR COMO MANIPULAR AS TABELAS DO BANCO DE DADOS
-    app.run(debug=True)
+    app.run(debug=IS_DEVELOPMENT)
+
+
+if __name__ == "__main__":
+    main()
